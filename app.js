55578716cfde60442320dda1894629ca3ac2717e
Nbahrom <nbahrom.1nurmurodov@gmail.com>
2021-05-28 09:08:59 +0500

    security section finish

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const morgan = require('morgan');
 
-const AppError = require('./utils/appError')
-const globalErrorHandler = require('./controllers/errorController')
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
@@ -11,25 +15,36 @@ const userRouter = require('./routes/userRouter');
 const app = express();
 
 // 1) Global Middlewares
+
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 const limiter = rateLimit({
   max: 1000,
-  windowMs: 60*60*1000,
-  message: "Too many request from this IP, please try again in an hour!"
-})
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!'
+});
 
-app.use('/api',limiter)
+app.use('/api', limiter);
 
+// for limit data app.use(express.json({limit: '10kb'}));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(hpp({
+  whitelist: ['duration']
+}));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -39,7 +54,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', ((req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`),404);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
 }));
 
 app.use(globalErrorHandler);
