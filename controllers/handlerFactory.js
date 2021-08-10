8332f7acd3506d7c86ad81fe92d3529f6b5c65f6
Bahrom  Nurmurodov <nbahrom.1nurmurodov@gmail.com>
2021-08-10 09:49:35 +0500

    calcAverageRatings

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -72,6 +72,7 @@ exports.getAll = Model => catchAsync(async (req, res) => {
     .limitFields()
     .paginate();
 
+  // const document = await features.query.explain();
   const document = await features.query;
 
   res.status(200).json({
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel')
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -36,13 +37,38 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.pre(/^find/, function(next) {
   this.populate({
     path: 'user',
-    select: { name: 1,photo: 1, '_id': 0}
+    select: { name: 1, photo: 1, '_id': 0 }
   }).populate({
     path: 'tour',
-    select: { guides: 0, name: 1}
-  })
+    select: { guides: 0, name: 1 }
+  });
   next();
 });
+
+
+reviewSchema.statics.calcAverageRatings = async function(touId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: touId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  await Tour.findByIdAndUpdate(touId, {
+    ratingsAverage: stats[0].avgRating,
+    ratingsQuantity: stats[0].nRating
+  })
+};
+
+reviewSchema.post('save', function() {
+  this.constructor.calcAverageRatings(this.tour)
+})
+
 
 const Review = mongoose.model('Review', reviewSchema);
 
