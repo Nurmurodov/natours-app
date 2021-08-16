62e796130f632acee1a504878c8ad47f64f34e2a
Bahrom  Nurmurodov <nbahrom.1nurmurodov@gmail.com>
2021-08-16 15:26:48 +0500

    overview Pages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
@@ -12,10 +13,16 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 const reviewRouter = require('./routes/reviewRouter');
+const viewRouter = require('./routes/viewRouter');
 
 const app = express();
 
 // 1) Global Middlewares
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(helmet());
 
@@ -33,7 +40,6 @@ app.use('/api', limiter);
 
 // for limit data app.use(express.json({limit: '10kb'}));
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
 app.use(mongoSanitize());
 
@@ -51,6 +57,7 @@ app.use((req, res, next) => {
 });
 
 // 3) Route
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
